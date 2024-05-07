6e809a91490db37c59aed58aa1ffd3ec0dc571f3
joao.cordeiro <joaocordeiro2134@gmail.com>
2024-05-07 17:07:08 -0300

    Add melhorias

--- src/views/author/Submit.vue
+++ src/views/author/Submit.vue
@@ -109,14 +109,6 @@ import { eventDetails } from '../../hooks/useEvent';
 import Swal from "sweetalert2"
 import Spinner from "../../components/Spinner.vue"
 
-const Toast = Swal.mixin({
-    toast: true, position: "top-end", showConfirmButton: false, timer: 3000, timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
-    }
-});
-
 export default defineComponent({
     setup(){
         const state: IArticleState = reactive({
