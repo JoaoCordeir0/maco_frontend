66d81f4874e72aab3a9edac721c5767b2bf62a33
JoaoCordeir0 <joaocordeiro2134@gmail.com>
2024-09-30 10:44:37 -0300

    melhoria

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <img src="/logo_maco.png" title="Maco: Magnoli & Cordeiro" alt="Logo Maco" width="150" class="hidden lg:block top-0 pl-2 pt-2 rounded-full" style="margin-bottom: -145px;">
     <div class="flex items-center justify-center h-screen px-3 bg-zinc-100">
-        <div class="w-full max-w-xl md:h-auto overflow-y-auto bg-white border-2 rounded-xl border-gray py-2 px-2 md:py-4 md:px-4 mt-2">            
+        <div class="w-full max-w-xl md:h-auto overflow-y-auto bg-white border-t-2 shadow-xl rounded-xl border-gray py-2 px-2 md:py-4 md:px-4 mt-2">            
             <form class="p-5 m-auto overflow-y-auto" @submit.prevent="sendRegister">
                 <div class="space-y-2 flex mb-2">
                     <a href="#" onclick="history.go(-1)" class="mt-3 mr-3"><font-awesome-icon :icon="['fas', 'arrow-left']" /></a>
@@ -59,17 +59,25 @@
                 <div class="grid grid-cols-12 gap-3 mt-3">
                     <div class="col-span-12 md:col-span-6">
                         <span class="text-sm text-gray-800">Senha <span class="text-red-500 font-semibold">*</span></span>
-                        <input type="password"
-                            maxlength="20"
-                            class="block w-full mt-1 border-gray-300 rounded-md focus:border-gray-800 focus:ring focus:ring-opacity-40 focus:ring-gray-800"
-                            v-model="pass1" />
+                        <div class="relative w-full">
+                            <input :type="type_input_1 ? 'text' : 'password'"
+                                class="block w-full mt-1 border-gray-300 rounded-md focus:border-gray-800 focus:ring focus:ring-opacity-40 focus:ring-gray-800"
+                                v-model="pass1" />
+                            <button class="absolute inset-y-0 right-0 pr-3 flex items-center" type="button" @click="type_input_1 = !type_input_1">
+                                <font-awesome-icon :icon="['fas', 'eye']" class="icon-color" />
+                            </button>  
+                        </div>                      
                     </div>
                     <div class="col-span-12 md:col-span-6">
                         <span class="text-sm text-gray-800">Confirme a senha <span class="text-red-500 font-semibold">*</span></span>
-                        <input type="password"
-                            maxlength="20"
-                            class="block w-full mt-1 border-gray-300 rounded-md focus:border-gray-800 focus:ring focus:ring-opacity-40 focus:ring-gray-800"
-                            v-model="pass2" />
+                        <div class="relative w-full">
+                            <input :type="type_input_2 ? 'text' : 'password'"
+                                class="block w-full mt-1 border-gray-300 rounded-md focus:border-gray-800 focus:ring focus:ring-opacity-40 focus:ring-gray-800"
+                                v-model="pass2" />
+                            <button class="absolute inset-y-0 right-0 pr-3 flex items-center" type="button" @click="type_input_2 = !type_input_2">
+                                <font-awesome-icon :icon="['fas', 'eye']" class="icon-color" />
+                            </button>  
+                        </div>
                     </div>
                 </div>
 
@@ -125,6 +133,8 @@ export default defineComponent({
         const pass2 = ref("")
         const course = ref("")
         const courses = ref()
+        const type_input_1 = ref(false)
+        const type_input_2 = ref(false)
 
         async function sendRegister(){                      
             if (name.value == '' || lastname.value == '' || cpf.value == '' || email.value == '' || ra.value == '' || pass1.value == '' || pass2.value == '' || course.value == '') {
@@ -167,6 +177,8 @@ export default defineComponent({
             course,   
             isLoading,
             courses,
+            type_input_1,
+            type_input_2,
             sendRegister,            
         }
     },
